c8a6cb0952a7ae44a3e09648e404b7bf392b63c3
Mirnadev <abdallahbamanamirna@gmail.com>
2026-06-27 12:42:38 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} est connecté !")
+    try:
+        synced = await bot.tree.sync()
+        print(f"{len(synced)} commandes synchronisées.")
+    except Exception as e:
+        print(e)
+
+@bot.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.text_channels, name="bienvenue")
+    if channel:
+        await channel.send(
+            f"🐉 Bienvenue {member.mention} sur **Éclipse Des Dragons** ! 💜"
+        )
+
+@bot.tree.command(name="ping", description="Vérifie si le bot est en ligne.")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("🏓 Pong ! Je suis bien en ligne.")
+
+bot.run(TOKEN)
